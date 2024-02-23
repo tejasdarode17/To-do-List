a2c6cf053a8c57b233c4c5f433d82c98c3f8ca44
Tejas Darode <123544186+tejasdarode17@users.noreply.github.com>
2024-02-23 14:41:34 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,27 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+const myButton = document.querySelector('button')
+
+myButton.addEventListener('click', function(e) {
+    if (inputBox.value === '') {
+        alert('Please add Something')
+    }else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.append(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.append(span)
+    }
+
+    inputBox.value = '';
+});
+
+listContainer.addEventListener('click' ,function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+    }
+    else if (e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove()
+    }
+}, false)
